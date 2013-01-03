f6593cd17d617f3e85af8eb7f042cb201012fc8f
ankat <ankat@ankat-R610>
2013-01-03 18:39:11 -0300

    Amélioration de la classe Bullet + création méthode drawBullet()

--- src/acdefender/Bullet.java
+++ src/acdefender/Bullet.java
@@ -4,21 +4,47 @@
  */
 package acdefender;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  *
  * @author ankat
  */
-public class Bullet {
+public class Bullet implements Runnable, ConstantesDefender {
     
-    final int posXDepartBullet = 0 ;
-    final int posYDepartBullet = (600 / 2 ) - 20 ;
-    final int tailleBullet = 20 ;
+    private int posXDepartBullet = X_DEPART_BULLET ;
+    private int posYDepartBullet = Y_DEPART_BULLET ;
     private int posXBullet ;
     private int posYBullet ;
     
     public Bullet(){
-        // TODO
-    }
+        posXBullet = posXDepartBullet ;
+        posYBullet = posYDepartBullet ;
+    } // fin constructeur
+    
+    /**
+     * Dessine la Bullet
+     * @param g : Graphics, le contexte graphic
+     * @param x : int, coordonnées x de la bullet
+     * @param y : int, coordonnées y de la bullet
+     */
+    public void drawBullet(Graphics g, int x, int y){
+        
+        if(DEBUG){
+            System.out.println("x == " + x + "... y == " + y); // DEBUG
+        }
+        g.setColor(Color.BLUE);
+        g.fillOval(x, y, TAILLE_BULLET, TAILLE_BULLET);
+        
+        
+    } // fin méthode drawBullet
+    
+    
+    @Override
+    public void run() {
+        
+    } // fin méthode run
     
     //--------------------------------------------------------------------------
     //                      GETTERS AND SETTERS
@@ -39,6 +65,8 @@ public class Bullet {
     public void setPosYBullet(int posYBullet) {
         this.posYBullet = posYBullet;
     }
+
+    
     
     
     
