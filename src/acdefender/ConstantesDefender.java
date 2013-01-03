14f4f9115693b0abaf809c609721834bb7129d91
ankat <ankat@ankat-R610>
2013-01-03 18:08:23 -0300

    Interface ConstantesDefender

---
+++ src/acdefender/ConstantesDefender.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acdefender;
+
+/**
+ * Interface contenant les constantes du jeu
+ * @author ankat
+ */
+public interface ConstantesDefender {
+    
+    static final int FRAME_WIDTH = 800 ;
+    static final int FRAME_HEIGHT = 600 ;
+    static String TITRE = "AC DEFENDER" ;
+    
+    static final boolean DEBUG = false;
+    
+}
