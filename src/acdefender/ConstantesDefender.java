71fb0ad3dbed1dc98ad9c55bbebc409d9edff38d
ankat <ankat@ankat-R610>
2013-01-03 18:28:02 -0300

    Ajout des constantes de Bullet dans l'interface contenant les constantes

--- src/acdefender/ConstantesDefender.java
+++ src/acdefender/ConstantesDefender.java
@@ -16,4 +16,8 @@ public interface ConstantesDefender {
     
     static final boolean DEBUG = false;
     
+    static final int TAILLE_BULLET = 20 ;
+    static final int X_DEPART_BULLET = 0 ;
+    static final int Y_DEPART_BULLET = (FRAME_HEIGHT / 2) - TAILLE_BULLET ;
+    
 }
