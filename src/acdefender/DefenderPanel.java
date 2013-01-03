8a46f3df8e3f566a0a4f4e6769f4d63dccac1ac9
ankat <ankat@ankat-R610>
2013-01-03 01:12:17 -0300

    fixes #1 Faire apparaître les monstres

--- src/acdefender/DefenderPanel.java
+++ src/acdefender/DefenderPanel.java
@@ -14,7 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 /**
- *
+ * Panneau du jeu qui est aussi le contrôleur
  * @author ankat
  */
 public class DefenderPanel extends JPanel implements Runnable, ConstantesDefender {
