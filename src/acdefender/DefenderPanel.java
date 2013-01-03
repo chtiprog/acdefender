25e5c7684e0f5b5fcf8e8cf05e46f1b7d0504278
ankat <ankat@ankat-R610>
2013-01-03 16:59:06 -0300

    Résout #2 Faire avancer les monstres

--- src/acdefender/DefenderPanel.java
+++ src/acdefender/DefenderPanel.java
@@ -19,42 +19,13 @@ import javax.swing.Timer;
  */
 public class DefenderPanel extends JPanel implements Runnable, ConstantesDefender {
 
-    /**
-     * Thread principal
-     */
-    Thread t;
-    /**
-     * Une instance de la classe Monstre
-     */
-    Monstre monstre;
-    /**
-     * Coordonnée x du Monstre
-     */
-    private int xMonstre;
-    /**
-     * Coordonnée y du Monstre
-     */
-    private int yMonstre;
-    /**
-     * Delai pour l'exécution de la tâche qui fait apparaître les Monstres
-     */
-    private int delay;
-    /**
-     * Contexte graphique
-     */
-    Graphics graph;
-    /**
-     * Un ActionListener permettant l'apparition des monstres
-     */
-    TaskPerformer taskPerformer;
-    /**
-     * Une liste contenant des monstres
-     */
-    ArrayList<Monstre> listeMonstres = new ArrayList<Monstre>();
-    /**
-     * Thread pour la méthode goMonstre
-     */
-    ThreadGoMonstre tMonstre;
+    
+    Thread t;           // Thread principal
+    Monstre monstre;    // Une instance de la classe Monstre
+    private int delay;  // Delai pour l'exécution de la tâche qui fait apparaître les Monstres
+    Graphics graph;     // Contexte graphique
+    TaskPerformer taskPerformer; // Un ActionListener permettant l'apparition des monstres
+    ArrayList<Monstre> listeMonstres = new ArrayList<Monstre>(); // Une liste contenant des monstres
 
     public DefenderPanel() {
         this.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
@@ -63,6 +34,7 @@ public class DefenderPanel extends JPanel implements Runnable, ConstantesDefende
         t.start();
     } // fin constructeur
 
+    @Override
     public void paintComponent(Graphics g) {
         g.setColor(Color.white);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
@@ -72,37 +44,26 @@ public class DefenderPanel extends JPanel implements Runnable, ConstantesDefende
             int posX = listeMonstres.get(i).getPosXMonstres();
             int posY = listeMonstres.get(i).getPosYMonstres();
             listeMonstres.get(i).drawMonstres(g, posX, posY);
+            
+            // Move monster for next place
+            listeMonstres.get(i).run();
         }
+        
+        // Après avoir placer tous les monstres on repeint la fenêtre
+        repaint();
 
-
-        System.out.println("taille listeMonstres : " + listeMonstres.size()); // DEBUG
+        if(DEBUG){
+            System.out.println("taille listeMonstres : " + listeMonstres.size()); // DEBUG
+        }
     } // fin méthode paintComponent
-
-    public void goMonstres() {
-
-        for (int j = 0; j < listeMonstres.size(); ++j) {
-            xMonstre = listeMonstres.get(j).getPosXMonstres();
-            for (int i = FRAME_WIDTH; i >= 0; --i) {
-                xMonstre = i;
-                listeMonstres.get(j).setPosXMonstres(xMonstre);
-                repaint();
-                try {
-                    Thread.sleep(10); // Pour poser le programme
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } // fin for i
-        } // fin for j
-    } // fin méthode goMonstres
 
     @Override
     public void run() {
-
-        delay = 2000;
+        delay = 1000;
         new Timer(delay, taskPerformer).start();
-
     }
 
+    
     /**
      * Tâche qui permet de faire apparaître des monstres en créant des nouveaux
      * monstres et en les ajoutant à la liste
@@ -113,23 +74,16 @@ public class DefenderPanel extends JPanel implements Runnable, ConstantesDefende
         public void actionPerformed(ActionEvent e) {
             monstre = new Monstre();
             listeMonstres.add(monstre);
-            tMonstre = new ThreadGoMonstre();
-            tMonstre.run();
-            repaint();
-            try {
-                Thread.sleep(10); // Pour poser le programme
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
+            new Thread (monstre).start();
+            
+//            try {
+//                Thread.sleep(1); // Pour poser le programme
+//            } catch (InterruptedException ie) {
+//                System.err.println("Erreur: Thread.sleep(5) du actionPerformed"+
+//                                   " --> TaskPerformer");
+//            }
         }
     } // fin classe TaskPerformer
-
-    public class ThreadGoMonstre implements Runnable {
-
-        @Override
-        public void run() {
-            goMonstres();
-            System.out.println("Je suis dans ThreadGoMonstre"); // DEBUG
-        }
-    }
+    
+    
 } // fin classe DefenderPanel
